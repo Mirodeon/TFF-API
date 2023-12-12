94b2d83a20c5ea3a83f5087291dbad249b9c8248
Mirodeon <mehdi.habaru@gmail.com>
2023-12-12 15:55:21 +0100

    add migrotions folders

--- build.sh
+++ build.sh
@@ -4,6 +4,5 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py makemigrations
-python manage.py auth --run-syncdb
 python manage.py migrate --run-syncdb
 python manage.py createsu
