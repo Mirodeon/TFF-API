f1d5bb66c295fb2dfc9289954dcafc9ff4ce861f
Mirodeon <mehdi.habaru@gmail.com>
2023-12-12 15:50:44 +0100

    add migrate auth to buildcommands

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py makemigrations
+python manage.py auth --run-syncdb
 python manage.py migrate --run-syncdb
 python manage.py createsu
