11359e2ad96697d45f32ea09d84c60e20fe8eeee
Mirodeon <mehdi.habaru@gmail.com>
2023-12-12 15:31:16 +0100

    up

--- TFF/settings.py
+++ TFF/settings.py
@@ -170,8 +170,6 @@ MEDIA_ROOT = BASE_DIR / 'mediafiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 AUTH_USER_MODEL = 'core_user.User'
 
 REST_FRAMEWORK = {
