28320840b3e5f953a7d746914682a3b890efc726
Mirodeon <mehdi.habaru@gmail.com>
2024-01-25 10:43:20 +0100

    add cors allow header

--- TFF/settings.py
+++ TFF/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 import dj_database_url
 import cloudinary
+from corsheaders.defaults import default_headers
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -197,5 +198,9 @@ SIMPLE_JWT = {
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = []
+CORS_ALLOW_HEADERS = (
+    *default_headers,
+    "x-api-key",
+)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
